319961358a075e87c5f8d13927b7dff2ce2af61c
Chester Rivas <chester.rivas@gmail.com>
2014-01-09 21:20:25 -0500

    updating copyright

--- js/plugins/jquery.owldropdown-1.3.js
+++ js/plugins/jquery.owldropdown-1.3.js
@@ -1,9 +1,10 @@
-/*
- * Owl Dropdown
- * crivas.net
- *
- * Author: Chester Rivas
- * Version: 1.2
+/*!
+ * pluginName: OwlDropDown
+ * author: Chester Rivas
+ * website: crivas.net
+ * description: jquery plugin for dropdown/combobox support, customizable actions and events
+ * version: 1.3
+ * Copyright (c) 2014 Crivas Inc.
  */
 
 var Owl = Owl || {};
@@ -26,7 +27,7 @@ $.fn.owldropdown = function(options) {
 		listItemElement: 'ul',
 		changeLabelOnItemClick: true,
 		inLineStyling: true,
-		clickAway: true,
+		clickAway: false,
 		closeWhenSelected: true,
 		enableArrowKeys: false,
 		labelWidth: 160,
@@ -58,30 +59,17 @@ $.fn.owldropdown = function(options) {
 		$this.addClass(owlDropdownClassName);
 		$this.find(settings.labelElement).addClass(owlLabelClassName);
 		$this.find(settings.listItemElement).addClass(owlListClassName);
-		if (settings.inLineStyling) {
-			$this.css({ // list
-				width: settings.labelWidth
-			});
-		}
-		if (settings.inLineStyling) {
-			$this.find(labelSelector).css({ // label <p>
-				zIndex: '9991',
-				position: 'relative'
-				//width: settings.labelWidth,
-				//height: settings.labelHeight,
-				//lineHeight: typeof settings.labelHeight == 'number' ? settings.labelHeight + 'px' : settings.labelHeight
-			});
-		}
-		$this.find(listItemSelector).css({ // list <ul>
-			zIndex: '9992',
-			position: 'relative'
+		$this.css({ // list
+			width: settings.labelWidth
 		});
-		if (settings.inLineStyling) {
-			$this.find(listItemSelector).find('li').css({ // list item <li>
-				//width: settings.dropDownWidth,
-				//height: settings.dropDownHeight
-			});
-		}
+		$this.find(labelSelector).css({ // label <p>
+			zIndex: findHighestZIndex()
+		});
+		$this.find(listItemSelector).css({ // list <ul>
+			zIndex: findHighestZIndex()
+		});
+		$this.find(listItemSelector).find('li').css({ // list item <li>
+		});
 
 		$this.find(listItemSelector).find('li').each(function(i){
 			$(this).attr('buttonID', i);
@@ -96,11 +84,6 @@ $.fn.owldropdown = function(options) {
 			$this.find(listItemSelector).prepend(prependElement);
 		}
 
-		//console.log('settings.defaultLabel', typeof settings.defaultLabel);
-		//console.log('settings.defaultValue', typeof settings.defaultValue);
-		//console.log('settings.labelSelector', typeof settings.labelSelector);
-		//console.log('settings.allowResetToDefault', typeof settings.allowResetToDefault);
-
 		if (typeof settings.defaultLabel !== 'undefined') {
 			$this.find(labelSelector).text(settings.defaultLabel);
 		}
@@ -111,24 +94,24 @@ $.fn.owldropdown = function(options) {
 	};
 
 	var getDefaultValue = function() {
-		return typeof settings.defaultValue !== 'null' ? settings.defaultValue : settings.defaultLabel;
+		return typeof settings.defaultValue !== 'undefined' ? settings.defaultValue : settings.defaultLabel;
 	};
 
 	var toggleDropDown = function() {
 		menuOpen = !menuOpen;
+		$this.toggleClass('open');
+		$this.find(listItemSelector).toggleClass('open');
+		$this.find(labelSelector).toggleClass('open');
 		if (menuOpen) {
 			openDropDown();
 		} else {
 			closeDropDown();
 		}
+		return false;
 	};
 
 	var openDropDown = function() {
 		menuOpen = true;
-		if (!$this.find(listItemSelector).hasClass('open')) {
-			$this.find(listItemSelector).addClass('open');
-			$this.find(labelSelector).addClass('open');
-		}
 		if (settings.clickAway) {
 			if (!$('.bg-clickaway').length) {
 				$('body').append("<div class='bg-clickaway'></div>"); // create element on body
@@ -136,60 +119,58 @@ $.fn.owldropdown = function(options) {
 					width: '200%',
 					height: '200%',
 					position: 'fixed',
-					backgroundColor: 'rgba(0,0,0,.9)',
+					backgroundColor: 'rgba(0,0,0,0.8)',
 					display: 'block',
 					left: '0px',
 					top: '0px',
-					zIndex: '9990'
+					zIndex: findHighestZIndex() - 5
 				});
 				$('div.bg-clickaway').on('click', closeDropDown); // add handler for background clicking
 			}
+			//$this.off('click'); // remove handler for entire element click
 		}
-		$this.off('click'); // remove handler for entire element click
-		$this.find(labelSelector).on('click', closeDropDown);  // add handlers to close dropdown
+		//$this.find(labelSelector).on('click', closeDropDown);  // add handlers to close dropdown
 		/*
-		if (settings.enableArrowKeys) {
-		$(document).on('keydown', function (e) {
-		console.log('keyCode', e.keyCode);
-		if (e.keyCode == 38) {
-		currentItemNumber -= 1;
-		var labelValue = $this.find(listItemSelector).find('li')[currentItemNumber].text();
-		console.log(labelValue);
-		$this.find(labelSelector).text(labelValue);
-		e.stopPropagation();
-		} else if (e.keyCode == 40) {
-		var labelValue = $this.find(listItemSelector).find('li')[currentItemNumber].text();
-		console.log(labelValue);
-		$this.find(labelSelector).text(labelValue);
-		currentItemNumber += 1;
-		e.stopPropagation();
-		}
-		});
-		}
-		*/
+		 if (settings.enableAsrrowKeys) {
+		 $(document).on('keydown', function (e) {
+		 console.log('keyCode', e.keyCode);
+		 if (e.keyCode == 38) {
+		 currentItemNumber -= 1;
+		 var labelValue = $this.find(listItemSelector).find('li')[currentItemNumber].text();
+		 console.log(labelValue);
+		 $this.find(labelSelector).text(labelValue);
+		 e.stopPropagation();
+		 } else if (e.keyCode == 40) {
+		 var labelValue = $this.find(listItemSelector).find('li')[currentItemNumber].text();
+		 console.log(labelValue);
+		 $this.find(labelSelector).text(labelValue);
+		 currentItemNumber += 1;
+		 e.stopPropagation();
+		 }
+		 });
+		 }
+		 */
 		$this.trigger(Owl.event.DROPDOWNOPENED);
+		return false;
 	};
 
 	var closeDropDown = function() {
 		menuOpen = false;
-		if ($this.find(listItemSelector).hasClass('open')) {
-			$this.find(listItemSelector).removeClass('open');
-			$this.find(labelSelector).removeClass('open');
-		}
 		if ($('.bg-clickaway').length) $('.bg-clickaway').remove(); // remove background click away element
-		//$this.find(listItemSelector).find('li').off('click'); // remove handlers for for item clicking
+		$this.removeClass('open');
+		$this.find(listItemSelector).removeClass('open');
+		$this.find(labelSelector).removeClass('open');
 		$this.on('click', openDropDown); // re-add open dropdown handler
 		/*
-		if (settings.enableArrowKeys) {
-		$(document).off('keydown');
-		}
-		*/
+		 if (settings.enableArrowKeys) {
+		 $(document).off('keydown');
+		 }
+		 */
 		$this.trigger(Owl.event.DROPDOWNCLOSED);
+		return false;
 	};
 
 	var itemClicked = function(e) {
-		console.log('itemClicked');
-		//var itemTextValue = $(e.currentTarget).text().replace(/\s+/g, ''),
 		var itemTextValue = $(e.currentTarget).text().trim(),
 			itemIDValue = $(e.currentTarget).attr('buttonID');
 
@@ -203,6 +184,21 @@ $.fn.owldropdown = function(options) {
 		}
 		if (settings.closeWhenSelected) closeDropDown(); // close dropdown when item is clicked
 		e.stopPropagation();
+		return false;
+	};
+
+	var findHighestZIndex = function(elem) {
+		var elems = document.getElementsByTagName(elem);
+		var highest = 100;
+		for (var i = 0; i < elems.length; i++)
+		{
+			var zindex=document.defaultView.getComputedStyle(elems[i],null).getPropertyValue("z-index");
+			if ((zindex > highest) && (zindex != 'auto'))
+			{
+				highest = zindex;
+			}
+		}
+		return highest;
 	};
 
 	initDropdown();
