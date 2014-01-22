9db4d2f119329ce941bf8b480c7f495767c7fa89
Chester Rivas <chester.rivas@gmail.com>
2014-01-21 21:25:16 -0500

    updated dropdown version and updated to an absolute div within a relative container

--- js/internal/Crivas.Documentation.js
+++ js/internal/Crivas.Documentation.js
@@ -1,26 +1,72 @@
+/*
+defaultLabel: undefined,
+defaultValue: undefined,
+innerListItemSelector: undefined,
+allowResetToDefault: false,
+labelElement: 'p',
+listItemElement: 'ul',
+changeLabelOnItemClick: true,
+inLineStyling: true,
+clickAway: false,
+closeWhenSelected: true,
+enableArrowKeys: false,
+labelWidth: 160,
+labelHeight: 25,
+dropDownWidth: 160,
+dropDownHeight: 25,
+defaultIndex: 0
+*/
 
 CS.documentation = {
 
     options: [
+	    {
+		    key: 'defaultLabel',
+		    defaultValue: 'undefined',
+		    type: 'N/A',
+		    description: 'N/A',
+		    required: false
+	    },
+	    {
+		    key: 'defaultValue',
+		    defaultValue: 'undefined',
+		    type: 'N/A',
+		    description: 'N/A',
+		    required: false
+	    },
+	    {
+		    key: 'defaultIndex',
+		    defaultValue: 0,
+		    type: 'N/A',
+		    description: 'N/A',
+		    required: false
+	    },
+	    {
+		    key: 'innerListItemSelector',
+		    defaultValue: 'undefined',
+		    type: 'String | Selector',
+		    description: 'A JQuery selector which is used to get and set the value within a list item',
+		    required: false
+	    },
 		{
 			key: 'labelElement',
 			defaultValue: 'p',
-			type: 'N/A',
+			type: 'String',
 			description: 'N/A',
 			required: false
 		},
 		{
 			key: 'dropDownElement',
 			defaultValue: 'ul',
-			type: 'N/A',
+			type: 'String',
 			description: 'N/A',
 			required: false
 		},
 		{
 			key: 'changeLabelOnItemClick',
-			defaultValue: 'false',
-			type: 'N/A',
-			description: 'N/A',
+			defaultValue: 'true',
+			type: 'Boolean',
+			description: 'Whether to change the label when a drop down value is selected.',
 			required: false
 		},
 		{
@@ -38,22 +84,29 @@ CS.documentation = {
 			required: false
 		},
 		{
-			key: 'componentWidth',
-			defaultValue: '100',
+			key: 'labelWidth',
+			defaultValue: '160',
 			type: 'N/A',
 			description: 'N/A',
 			required: false
 		},
 		{
-			key: 'componentHeight',
-			defaultValue: '35',
+			key: 'labelHeight',
+			defaultValue: '25',
+			type: 'N/A',
+			description: 'N/A',
+			required: false
+		},
+		{
+			key: 'dropDownWidth',
+			defaultValue: '160',
 			type: 'N/A',
 			description: 'N/A',
 			required: false
 		},
 		{
 			key: 'dropdownHeight',
-			defaultValue: 'settings.componentHeight',
+			defaultValue: '25',
 			type: 'N/A',
 			description: 'N/A',
 			required: false
